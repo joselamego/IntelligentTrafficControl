723db523d1997229c99eae42723adc59a2a5ca90
Jose Lamego <jose.a.lamego@linux.intel.com>
2016-07-22 09:38:07 -0500

    set_server_ip: Fix pep8 compliance
    Some whitespaces and missed indentations do not comply with pep8.
    
    Signed-off-by: Jose Lamego <jose.a.lamego@linux.intel.com>

--- set_server_ip.py
+++ set_server_ip.py
@@ -1,11 +1,15 @@
 import logging
 from subprocess import check_output
-logging.basicConfig(filename='log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+logging.basicConfig(filename='log', format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
 
 def run():
     try:
-        server_ip=check_output("ifconfig wlan0 | awk \'/t addr:/{gsub(/.*:/,\"\",$2);print$2}\'", shell=True)
-        server_ip=server_ip.replace("\n", "")
+        server_ip = check_output(
+            "ifconfig wlan0 | awk \'/t addr:/{gsub(/.*:/,\"\",$2);print$2}\'",
+            shell=True)
+        server_ip = server_ip.replace("\n", "")
         logging.info('Server IP defined as: ' + str(server_ip))
         return server_ip
     except:
