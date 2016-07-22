ee5c4c98d0378c1a377d68009b9e7d77eab1d6b9
Jose Lamego <jose.a.lamego@linux.intel.com>
2016-07-22 09:40:38 -0500

    sync_time: Fix pep8 compliance
    Some whitespaces and missed indentations do not comply with pep8.
    
    Signed-off-by: Jose Lamego <jose.a.lamego@linux.intel.com>

--- sync_time.py
+++ sync_time.py
@@ -2,14 +2,19 @@ import logging
 import ntplib
 from time import strftime, localtime
 from os import system
-logging.basicConfig(filename='log', format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+logging.basicConfig(
+    filename='log', format='%(asctime)s %(message)s',
+    datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.DEBUG)
+
 
 def run():
     try:
         client = ntplib.NTPClient()
         response = client.request('north-america.pool.ntp.org')
-        system('date ' + strftime('%m%d%H%M%Y.%S',localtime(response.tx_time)))
-        logging.info('Time synchronized with north-america.pool.ntp.org server.')
+        system('date ' +
+               strftime('%m%d%H%M%Y.%S', localtime(response.tx_time)))
+        logging.info(
+            'Time synchronized with north-america.pool.ntp.org server.')
 
     except:
         logging.warning('Could not synchronize local time with ntp server.')
